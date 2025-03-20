808cbf70b9aa74d40172890a7c5eeeaa1fe7564d
karanx64 <karansinghx64@gmail.com>
2025-03-21 02:09:22 +0530

    header + explore menu (somewhat working)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,21 @@
-import "./App.css";
+import React from "react";
+import Navbar from "./components/navbar/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/home/Home";
+import Cart from "./Pages/cart/Cart";
+import PlaceOrder from "./Pages/placeOrder/PlaceOrder";
 
-function App() {
-  return <></>;
-}
+const App = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/PlaceOrder" element={<PlaceOrder />} />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
---
+++ frontend/src/components/foodItem/FoodItem.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./FoodItem.css";
+
+const FoodItem = ({id, name, price, description, image}) => {
+  return <div></div>;
+};
+
+export default FoodItem;
