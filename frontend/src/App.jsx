943c60700da8741c8bac880fba25305545dec05b
karanx64 <karansinghx64@gmail.com>
2025-03-23 03:48:13 +0530

    ui almost done, not responsive

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,17 +4,21 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/home/Home";
 import Cart from "./Pages/cart/Cart";
 import PlaceOrder from "./Pages/placeOrder/PlaceOrder";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
   return (
-    <div className="app">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/PlaceOrder" element={<PlaceOrder />} />
-      </Routes>
-    </div>
+    <>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/PlaceOrder" element={<PlaceOrder />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
   );
 };
 
