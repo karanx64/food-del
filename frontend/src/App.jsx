20b9403e661dfb4b77a859fe4bd94fadb0ee7965
karanx64 <karansinghx64@gmail.com>
2025-03-23 04:30:52 +0530

    fully responsive

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import "./App.css";
+
 import Navbar from "./components/navbar/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/home/Home";
