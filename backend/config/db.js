d3e378e995d5d0b57aa537ee3f24df1b3501dd2e
karanx64 <karansinghx64@gmail.com>
2025-03-23 18:37:00 +0530

    fixed DB connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,15 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  (
-    await mongoose.connect(
+  await mongoose
+    .connect(
       "mongodb+srv://karansinghx64:KrnMongox64@cluster0.q2dlr.mongodb.net/food-del"
     )
-  ).isObjectIdOrHexString(() => console.log("MongoDB Connected"));
+    .then(() => {
+      console.log("MongoDB atlas connected.");
+    });
 };
 
 export { connectDB };
+
+// "mongodb+srv://karansinghx64:KrnMongox64@cluster0.q2dlr.mongodb.net/food-del"
