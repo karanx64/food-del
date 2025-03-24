4533e4dba1a99e72106a42d46037b7772f559c96
karanx64 <karansinghx64@gmail.com>
2025-03-24 17:00:27 +0530

    admin changes

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/sidebar/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import Add from "./pages/add/Add";
+import List from "./pages/list/List";
+import Orders from "./pages/orders/Orders";
+import { ToastContainer } from "react-toastify";
 
 const App = () => {
-  return <div></div>;
+  const url = "http://localhost:4000";
+  return (
+    <div>
+      <ToastContainer />
+      <Navbar />
+      <hr />
+      <div className="app-content">
+        <Sidebar />
+        <Routes>
+          <Route path="/add" element={<Add url={url} />} />
+          <Route path="/list" element={<List url={url} />} />
+          <Route path="/orders" element={<Orders url={url} />} />
+        </Routes>
+      </div>
+    </div>
+  );
 };
 
 export default App;
---
+++ admin/src/pages/orders/Orders.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Orders.css";
+const orders = ({ url }) => {
+  return <div>orders</div>;
+};
+
+export default orders;
